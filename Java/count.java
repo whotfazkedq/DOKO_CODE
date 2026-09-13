30196501fff03feac105ab834b21b712eb5288de
whoazkedq <angel.gabriel.olvera.santos@gmail.com>
2026-09-12 23:36:21 -0600

    I think I solved it

--- Java/count.java
+++ Java/count.java
@@ -1,8 +1,10 @@
+package java2;
+
 import java.util.Scanner;
 import java.util.Random;
 import java.util.InputMismatchException;
 
-public class counter { 
+public class count { 
     public static void main(String[] args) throws InterruptedException {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
