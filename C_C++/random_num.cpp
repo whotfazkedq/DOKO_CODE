41cc98ce20e93d12d2ee75cda256fb2f1b3e9010
whoazkedq <angel.gabriel.olvera.santos@gmail.com>
2026-09-15 17:01:48 -0600

    just added a looping option to the random number generator

--- C_C++/random_num.cpp
+++ C_C++/random_num.cpp
@@ -10,5 +10,13 @@ int main(){
     scanf("%d", &max);
     num = min + rand() % (max - min + 1);
     printf("Your random number is: %d\n", num);
+    printf("Would you like to generate another random number? (y/n): ");
+    char choice;
+    scanf(" %c", &choice);
+    if(choice == 'y' || choice == 'Y'){
+        main();
+    } else {
+        return 0;
+    }
     return 0;
 }
