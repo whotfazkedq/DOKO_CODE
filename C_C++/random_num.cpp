728c142ccea7259ba9cfc7e046b8456270f996fc
whoazkedq <angel.gabriel.olvera.santos@gmail.com>
2026-09-15 17:24:54 -0600

    fixed wrong input errors

--- C_C++/random_num.cpp
+++ C_C++/random_num.cpp
@@ -1,22 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int readInt(const char* prompt){
+    int val;
+    int res;
+    int c;
+    while (1){
+        printf("%s", prompt);
+        res = scanf("%d", &val);
+        if (res == 1){
+            break;
+        }
+        printf("Invalid input, enter numbers only\n");
+        while ((c=getchar())!= '\n' && c != EOF);
+    }
+    return val;
+}
+
 int main(){
     int min, max, num;
+    char choice;
+    
     puts("Random Number Generator");
-    printf("Enter the minimum value: ");
-    scanf("%d", &min);
-    printf("Enter the maximum value: ");
-    scanf("%d", &max);
+    min = readInt("Enter the minimum value: ");
+    max = readInt("Enter the maximum value: ");
+
     num = min + rand() % (max - min + 1);
     printf("Your random number is: %d\n", num);
-    printf("Would you like to generate another random number? (y/n): ");
-    char choice;
-    scanf(" %c", &choice);
-    if(choice == 'y' || choice == 'Y'){
-        main();
-    } else {
-        return 0;
-    }
+    do{
+        
+        printf("Would you like to generate another random number? (y/n): ");
+        scanf(" %c", &choice);
+    
+        if(choice == 'y' || choice == 'Y'){
+            main();
+        } else if(choice == 'n' || choice == 'N'){
+            puts("Closing");
+        } else {
+            puts("Invalid choice, select yes or no");
+        }
+    } while (choice != 'y' && choice != 'Y' && choice != 'n' && choice != 'N');
     return 0;
 }
